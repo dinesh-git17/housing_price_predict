b70ccc484300b6cc500b9a98b35b055c219ee472
dinesh-git17 <dineshddawo@gmail.com>
2025-02-11 16:01:34 -0500

    added loading bar

--- model_tuning.py
+++ model_tuning.py
@@ -1,29 +1,25 @@
-# model_tuning.py
-
-from sklearn.ensemble import RandomForestRegressor
+from tqdm import tqdm
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
 
 def tune_model(X_train, y_train):
-    # Set up the RandomForestRegressor
+    # Define the model
     model = RandomForestRegressor()
 
-    # Define hyperparameters to tune
+    # Define the parameter grid
     param_grid = {
         'n_estimators': [100, 200],
-        'max_depth': [10, 20, None],
+        'max_depth': [10, 20],
         'min_samples_split': [2, 5],
-        'min_samples_leaf': [1, 2]
+        'min_samples_leaf': [1, 2],
+        'bootstrap': [True, False]
     }
 
-    # Set up GridSearchCV
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
+    # Set up GridSearchCV with a progress bar
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, n_jobs=-1)
 
-    # Fit the model
-    grid_search.fit(X_train, y_train)
+    # Wrap the fit method with tqdm to show a progress bar
+    # Use a tqdm loop to track the progress of the grid search
+    tqdm(grid_search.fit(X_train, y_train), desc="Grid Search Progress", total=grid_search.get_n_splits(), position=0)
 
-    # Get the best model and evaluation metrics
-    best_model = grid_search.best_estimator_
-    best_rmse = grid_search.best_score_
-    best_r2 = grid_search.best_estimator_.score(X_train, y_train)
-
-    return best_model, best_rmse, best_r2
+    return grid_search.best_estimator_
